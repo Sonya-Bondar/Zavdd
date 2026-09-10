808c2a5b7d6a44156c5704ce33b3f1cb44f4ac83
Sonya <sofabondar2111@icloud.com>
2026-09-10 17:01:34 +0300

    First commit: laba1.py

---
+++ laba1.py
@@ -0,0 +1,29 @@
+print("Hello World")
+name = "Шнек"
+age = 52
+haight = 1.67
+student = True
+sabjects = ["Math", "English", "History"]
+Bobik = (10,30)
+student_info = {
+    "name": "Шнек",
+    "age" : "52",
+    "haight": "1.67",
+}
+number = {1,2,3,4}
+print(name,type(name))
+print(age,type (age))
+print(haight,type(haight))
+print(student,type(student))
+print(sabjects,type(sabjects))
+print(student_info, type(student_info))
+print(number, type(number))
+print(Bobik,type(Bobik))
+print(22+12)
+print(22-12)
+print(22/12)
+print(22*12)
+print(22==12)
+print(22//12)
+print(22%12)
+print(22**2)
