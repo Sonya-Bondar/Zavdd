ffc75150a4b1fc05a8904d535d6c471e92191366
Sonya <sofabondar2111@icloud.com>
2026-09-15 20:12:20 +0300

    First commit: zav1.py

--- zav1.py
+++ zav1.py
@@ -1,25 +1,29 @@
-usrname= {
-    "Шнек" : {"password": "хотдаг","grades": [10,4,8,3,12,5]},
-    "Кені": {"password": "сахур","grades": [11,12,9,2,7]},
-    "Донателло": {"password": "черепаха","grades": [4,3,5,4,6,10]},
-    "Кайл": {"password": "гррмандейс","grades": [10,4,12,8,12,7]},
-    "Владік": {"password": "справжнійвовк","grades": [5,2,6,7,12,11,8]}
+print("Hello World")
+name = "Шнек"
+age = 52
+haight = 1.67
+student = True
+sabjects = ["Math", "English", "History"]
+Bobik = (10,30)
+student_info = {
+    "name": "Шнек",
+    "age" : "52",
+    "haight": "1.67",
 }
-login= input("Введіть логін:")
-password=input("Введіть пароль:")
-if login in usrname and usrname[login] ["password"]== password:
-    user=usrname[login]
-    grades= user["grades"]
-    print("Вхід успішний!")
-    print(f"Ваші оцінки: {grades} ")
-    zadovoleno=0
-    nezadovoleno=0
-    for grade in grades:
-        if 5<= grade <=12:
-            zadovoleno += 1
-        elif 1 <= grade <= 4:
-            nezadovoleno +=1
-    print(f"Кількість задовільних оцінок (5-12): {zadovoleno}")
-    print(f"Кількість незадовільних оцінок (1-4): {nezadovoleno}")
-else:
-    print("Помилка: невірний пароль або логін!")
+number = {1,2,3,4}
+print(name,type(name))
+print(age,type (age))
+print(haight,type(haight))
+print(student,type(student))
+print(sabjects,type(sabjects))
+print(student_info, type(student_info))
+print(number, type(number))
+print(Bobik,type(Bobik))
+print(22+12)
+print(22-12)
+print(22/12)
+print(22*12)
+print(22==12)
+print(22//12)
+print(22%12)
+print(22**2)
